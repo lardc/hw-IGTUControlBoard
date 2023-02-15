fbcdbfb807c853116af1ce49e1a5d12313d4e69d
d.ivanov <d.ivanov@proton-electrotex.com>
2023-02-15 13:20:17 +0300

    Некоторые функции переработаны или упрощены, т.к. был изменен подход к оцифровке параметров

--- Firmware/Source/Controller/Measurement.c
+++ Firmware/Source/Controller/Measurement.c
@@ -1,113 +1,67 @@
 ﻿// Includes
 #include "Measurement.h"
 #include "Board.h"
-#include "LowLevel.h"
 #include "DataTable.h"
 #include "Global.h"
 
 // Variables
-volatile Int16U MEASURE_V_CsensRaw[ADC_DMA_BUFF_SIZE_VGS_IGES];
-volatile Int16U MEASURE_V_VsensRaw[ADC_DMA_BUFF_SIZE_VGS_IGES];
-volatile Int16U MEASURE_C_Raw[ADC_DMA_BUFF_SIZE_QG];
+volatile Int16U MEASURE_V_CsensRaw[ADC_DMA_BUFF_SIZE_VGS_IGES] = {0};
+volatile Int16U MEASURE_V_VsensRaw[ADC_DMA_BUFF_SIZE_VGS_IGES] = {0};
+volatile Int16U MEASURE_C_Raw[ADC_DMA_BUFF_SIZE_QG] = {0};
+
+// Functions prototypes
+//
+float MEASURE_ExtractX(Int16U* InputArray, Int16U ArraySize);
+float MEASURE_ExtractVgsIges(Int16U* InputArray);
+void MEASURE_StartNewSampling();
 
 // Functions
 //
-
-Int16U MEASURE_PotSen()
+MeasureSample MEASURE_SampleVgsIges()
 {
-	return ADC_Measure(ADC3, ADC3_POT_CHANNEL);
+	MeasureSample Sample;
+
+	Sample.Voltage = CU_ADCtoV(MEASURE_DMAExtract(&MEASURE_V_VsensRaw[0]));
+	Sample.Current = CU_ADCtoI(MEASURE_DMAExtract(&MEASURE_V_CsensRaw[0]));
+	MEASURE_StartNewSampling();
+
+	return Sample;
 }
 //-----------------------------------------------
 
-Int16U MEASURE_V_VSen()
-{
-	return ADC_Measure(ADC1, ADC1_V_V_SEN_CHANNEL);
-}
-//-----------------------------------------------
-
-Int16U MEASURE_V_CSen()
-{
-	return ADC_Measure(ADC1, ADC1_V_C_SEN_CHANNEL);
-}
-//-----------------------------------------------
-
-Int16U MEASURE_C_VSen()
-{
-	return ADC_Measure(ADC1, ADC1_C_V_SEN_CHANNEL);
-}
-//-----------------------------------------------
-
-Int16U MEASURE_C_CSen()
-{
-	return ADC_Measure(ADC1, ADC1_C_C_SEN_CHANNEL);
-}
-//-----------------------------------------------
-
-Boolean MEASURE_VGS_Params(volatile RegulatorParamsStruct* Regulator, Int16U State)
-{
-	float V = (State == DS_Selftest) ? CU_V_ADCVToX(MEASURE_V_VSen()) : CU_PotADCVToX(MEASURE_PotSen());
-	float C = CU_V_ADCCToX(MEASURE_V_CSen());
-
-	if(Regulator->RegulatorStepCounter == 0)
-	{
-		V = 0;
-		C = 0;
-	}
-	Regulator->VSen = V;
-	Regulator->CSen = C;
-
-	// проверка на достижение током порогового значения
-	if((C >= (float)DataTable[REG_VGS_C_TRIG]) && (Regulator->CTrigRegulatorStep == 0))
-	{
-		Regulator->CTrigRegulatorStep = Regulator->RegulatorStepCounter;
-		Regulator->VSenForm[Regulator->RegulatorStepCounter] = (Int16U)V;
-		Regulator->CTrigVSen = V;
-		Regulator->CTrigCSen = C;
-		return true;
-	}
-	else
-		return false;
-}
-//-----------------------------------------------
-
-void MEASURE_IGES_Params(volatile RegulatorParamsStruct* Regulator, Int16U State)
-{
-	float V = (State == DS_Selftest) ? CU_V_ADCVToX(MEASURE_V_VSen()) : CU_PotADCVToX(MEASURE_PotSen());
-
-	if(Regulator->RegulatorStepCounter == 0)
-		V = 0;
-	Regulator->VSen = V;
-	Regulator->VSenForm[Regulator->RegulatorStepCounter] = V;
-}
-//-----------------------------------------------
-
-void MEASURE_C_DMABufferClear()
-{
-	//for(int i = 0; i < ADC_DMA_BUFF_SIZE; i++)
-	//	MEASURE_C_SenRaw[i] = 0;
-}
-//-----------------------------------------------
-
-Int16U MEASURE_C_DMAExtractCSen()
-{
-	//return MEASURE_Average((Int16U*)&MEASURE_C_SenRaw[0], ADC_DMA_BUFF_SIZE, ADC_NUMBER_OF_CHANNELS, ADC1_C_C_SEN_CHANNEL);
-}
-//-----------------------------------------------
-
-Int16U MEASURE_C_DMAExtractVSen()
-{
-	//return MEASURE_Average((Int16U*)&MEASURE_C_SenRaw[0], ADC_DMA_BUFF_SIZE, ADC_NUMBER_OF_CHANNELS, ADC1_C_V_SEN_CHANNEL);
-}
-//-----------------------------------------------
-
-Int16U MEASURE_Average(Int16U* InputArrayAddr, Int16U ArraySize, Int16U NumberOfChannels, Int16U Channel)
+float MEASURE_ExtractX(Int16U* InputArray, Int16U ArraySize)
 {
 	Int32U AverageData = 0;
 
-	for(int i = 0; i < ArraySize; i += NumberOfChannels)
-	{
-		AverageData += *(InputArrayAddr + (Channel - 1) + i);
-	}
+	for(int i = 0; i < ArraySize; i++)
+		AverageData += *(InputArray + i);
 
-	return (Int16U)((float)NumberOfChannels * AverageData / ArraySize);
+	return (float)AverageData / ArraySize;
 }
+//-----------------------------------------------
+
+float MEASURE_ExtractVgsIges(Int16U* InputArray)
+{
+	return MEASURE_DMAExtractX(InputArray, ADC_DMA_BUFF_SIZE_VGS_IGES - 1);
+}
+//-----------------------------------------------
+
+void MEASURE_StartNewSampling()
+{
+	DMA_TransferCompleteReset(DMA1, DMA_TRANSFER_COMPLETE);
+	DMA_TransferCompleteReset(DMA2, DMA_TRANSFER_COMPLETE);
+	ADC_SamplingStart(ADC1);
+	ADC_SamplingStart(ADC3);
+}
+//-----------------------------------------------
+
+float MEASURE_ExtractAveragedDatas(float* Buffer, Int16U BufferLength)
+{
+	float Temp = 0;
+
+	for(int i = 0; i < BufferLength; i++)
+		Temp += *(Buffer + i) / BufferLength;
+
+	return Temp;
+}
+//-----------------------------
--- Firmware/Source/Controller/Measurement.h
+++ Firmware/Source/Controller/Measurement.h
@@ -4,24 +4,24 @@
 // Includes
 #include "SysConfig.h"
 #include "ZwBase.h"
-#include "ConvertUtils.h"
-#include "Regulator.h"
 
-#define ADC_NUMBER_OF_CHANNELS		2
+// Types
+//
+typedef struct __MeasureSample
+{
+	float Current;
+	float Voltage;
+}MeasureSample;
+
 // Variables
+//
 extern volatile Int16U MEASURE_V_CsensRaw[ADC_DMA_BUFF_SIZE_VGS_IGES];
 extern volatile Int16U MEASURE_V_VsensRaw[ADC_DMA_BUFF_SIZE_VGS_IGES];
 extern volatile Int16U MEASURE_C_Raw[ADC_DMA_BUFF_SIZE_QG];
 
 // Functions
-Int16U MEASURE_V_VSen();
-Int16U MEASURE_V_CSen();
-Int16U MEASURE_C_CSen();
-Boolean MEASURE_VGS_Params(volatile RegulatorParamsStruct* Regulator, Int16U State);
-void MEASURE_IGES_Params(volatile RegulatorParamsStruct* Regulator, Int16U State);
-Int16U MEASURE_Average(Int16U* InputArrayAddr, Int16U ArraySize, Int16U NumberOfChannels, Int16U Channel);
-void MEASURE_C_DMABufferClear();
-Int16U MEASURE_C_DMAExtractCSen();
-Int16U MEASURE_C_DMAExtractVSen();
+//
+MeasureSample MEASURE_SampleVgsIges();
+float MEASURE_ExtractAveragedDatas(float* Buffer, Int16U BufferLength);
 
 #endif /* MEASUREMENT_H_ */
