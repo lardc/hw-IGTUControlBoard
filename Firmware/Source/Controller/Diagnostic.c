858e7e8d1f3020c96a1eae1acc2be68643de25f6
Pumukun <demgrisha@gmail.com>
2025-05-22 15:39:29 +0300

    Добавлен код для диагностики

---
+++ Firmware/Source/Controller/Diagnostic.c
@@ -0,0 +1,29 @@
+﻿// Header
+#include "Diagnostic.h"
+
+// Include
+#include "Delay.h"
+#include "LowLevel.h"
+#include "DataTable.h"
+#include "DeviceObjectDictionary.h"
+#include "Controller.h"
+
+// Functions
+//
+bool DIAG_HandleDiagnosticAction(Int16U ActionID, Int16U *pUserError)
+{
+	switch (ActionID)
+	{
+		case ACT_DBG_EXT_INDICATION:
+			LL_ExtIndication(true);
+			DELAY_MS(500);
+			LL_ExtIndication(false);
+			break;
+
+		default:
+			return false;
+	}
+
+	return true;
+}
+//-----------------------------
