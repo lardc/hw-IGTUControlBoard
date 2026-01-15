fb1bc4800ea040a2a2ec8dca926abdb3ff14c8ff
asytyi <alexander.a.sytyi@gmail.com>
2026-01-15 15:02:58 +0300

    Констрейнт для напряжения понижен до 0,2В

--- Firmware/Source/Platform/Constraints.h
+++ Firmware/Source/Platform/Constraints.h
@@ -116,7 +116,7 @@
 #define VGS_I_TRIG_MAX				300			// (мА)
 #define VGS_I_TRIG_DEF				300			// (мА)
 //
-#define VGS_V_DEST_MIN				2			// (В)
+#define VGS_V_DEST_MIN				0.2			// (В)
 #define VGS_V_DEST_MAX				30			// (В)
 #define VGS_V_DEST_DEF				10			// (В)
 //
